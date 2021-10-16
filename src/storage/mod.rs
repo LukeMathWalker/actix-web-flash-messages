33277875d992c910f927fa2236985cf1c1bb7077
LukeMathWalker <contact@lpalmieri.com>
2021-10-16 14:27:55 +0100

    Add proper feature flags.

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,8 +1,14 @@
 //! Pluggable storage backends for flash messages.
-mod cookies;
 mod interface;
-mod sessions;
 
+#[cfg(feature = "cookies")]
 pub use cookies::{CookieMessageStore, CookieMessageStoreBuilder};
+#[cfg(feature = "cookies")]
+mod cookies;
+
 pub use interface::{FlashMessageStore, LoadError, StoreError};
+
+#[cfg(feature = "sessions")]
+mod sessions;
+#[cfg(feature = "sessions")]
 pub use sessions::SessionMessageStore;
